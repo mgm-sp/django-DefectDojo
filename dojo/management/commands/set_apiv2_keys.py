65af53b14878fa2a6fa6aff18777576c67e456c7
Robert Knauer <robert.knauer@mgm-sp.com>
2020-11-25 16:32:52 +0100

    fix set_apiv2_keys command

--- dojo/management/commands/set_apiv2_keys.py
+++ dojo/management/commands/set_apiv2_keys.py
@@ -16,16 +16,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Get all APIv1 keys:
         for apiv1key in ApiKey.objects.all():
-            # Get or create corresponding APIv2 key:
-            apiv2key, _ = Token.objects.get_or_create(user=apiv1key.user)
+            # Get corresponding APIv2 key:
+            apiv2key = Token.objects.filter(user=apiv1key.user).first()
 
             # Print before state:
             print('BEFORE: User: %s, APIv1key: %s, APIv2key: %s' % (
-                apiv1key.user.username, apiv1key.key, apiv2key.key))
+                apiv1key.user.username, apiv1key.key,
+                'none' if apiv2key is None else apiv2key.key))
 
-            # Set APIv2 key to APIv1 key:
-            apiv2key.key = apiv1key.key
-            apiv2key.save()
+            # Delete old APIv2 key if it exists:
+            # NOTE: Updating it doesn't work because the `key` field is the
+            #       primary key, so we have to delete the old one and create
+            #       a new one.
+            if apiv2key is not None:
+                apiv2key.delete()
+
+            # Create new APIv2 key:
+            apiv2key = Token.objects.create(user=apiv1key.user,
+                                            key=apiv1key.key)
 
             # Print after state:
             print('AFTER: User: %s, APIv1key: %s, APIv2key: %s' % (
