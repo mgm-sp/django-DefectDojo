e043bffb269d743af371a32069d97109db147134
Robert Knauer <robert.knauer@mgm-sp.com>
2022-08-16 14:30:10 +0200

    Fix entrypoint-uwsgi.sh

--- docker/entrypoint-uwsgi.sh
+++ docker/entrypoint-uwsgi.sh
@@ -27,13 +27,11 @@ exec uwsgi \
   --processes ${DD_UWSGI_NUM_OF_PROCESSES:-2} \
   --threads ${DD_UWSGI_NUM_OF_THREADS:-2} \
   --wsgi dojo.wsgi:application \
-  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-32768}" \
-  --http 0.0.0.0:8081 --http-to ${DD_UWSGI_ENDPOINT} \
   --socket-timeout 86400 \
   --http-timeout 86400 \
   --harakiri 86400 \
-  --stats /run/defectdojo/stats.socket
-  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-8192}" \
+  --stats /run/defectdojo/stats.socket \
+  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-32768}" \
   --http 0.0.0.0:8081 --http-to ${DD_UWSGI_ENDPOINT} \
   --logformat "${DD_UWSGI_LOGFORMAT:-$DD_UWSGI_LOGFORMAT_DEFAULT}"
   # HTTP endpoint is enabled for Kubernetes liveness checks. It should not be exposed as a serivce.
